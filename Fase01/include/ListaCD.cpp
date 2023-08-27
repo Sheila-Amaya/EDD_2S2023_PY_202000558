b2f3786fb7a73f8489060158c85e95437c526e4d
Sheila <2851326790117@ingenieria.usac.edu.gt>
2023-08-27 01:08:45 -0600

    ...
    Se agregaron opciones al menu principal

--- Fase01/include/ListaCD.cpp
+++ Fase01/include/ListaCD.cpp
@@ -14,32 +14,29 @@ ListaCD::~ListaCD()
     //dtor
 }
 
-void ListaCD::Insertar(std::string codigo, std::string nombre) // tipo , clase ,nombre funcion
+void ListaCD::Insertar(std::string codigo, std::string nombre)
 {
-    NodoCD *nuevo = new NodoCD(codigo,nombre);
-    if(this->Primero == 0)
+    NodoCD *nuevo = new NodoCD(codigo, nombre);
+    if (this->Primero == 0)
     {
         nuevo->Anterior = nuevo;
         nuevo->Siguiente = nuevo;
         this->Primero = nuevo;
         this->Tamanio++;
-    }else
+    }
+    else
     {
-        NodoCD *aux = this->Primero;
-        int contador = 1;
-        while(this->Tamanio > contador)
-        {
-            aux = aux->Siguiente;
-            contador++;
-        }
+        NodoCD *aux = this->Primero->Anterior; //
         nuevo->Anterior = aux;
         nuevo->Siguiente = this->Primero;
-        aux->Siguiente = nuevo;
+        aux->Siguiente = nuevo; //
         this->Primero->Anterior = nuevo;
         this->Tamanio++;
     }
-
 }
+
+
+
 
 void ListaCD::mostrar()
 {
--- Fase01/main.cpp
+++ Fase01/main.cpp
@@ -1,18 +1,93 @@
 #include <iostream>
 
-#include "ListaCD.h" //importa la clase
+//#include "ListaCD.h" //importa la clase
 
 using namespace std;
 
+//
+
+
+bool iniciarSesion();
+void mostrarMenu();
+
 int main()
 {
-    //instancia de la lista
-
-    ListaCD *Lista = new ListaCD();
-    Lista->Insertar("BDEV-001","HOLA");
-    Lista->Insertar("BDEV-002","HOLA");
-    Lista->Insertar("BDEV-003","HOLA");
-    Lista->mostrar();
+    if (iniciarSesion()) {
+        cout << "\n\tsuccessfully logged in.. welcome\n" << endl;
+        mostrarMenu();
+    }
 
     return 0;
 }
+
+//funcion para iniciar sesion
+bool iniciarSesion()
+{
+    string username;
+    string password;
+
+    do {
+        cout << "\n\t=============      EDD PROJECT-UP     =============" << endl;
+        cout << "\tUSUARIO: " ;
+        cin >> username;
+
+        cout << "\tPASSWORD: " ;
+        cin >> password;
+
+
+
+        if (username == "PM-202000558" && password == "pmPassword123") {
+            return true;
+        } else {
+            cout << "\n\tpassword o username incorrectos, intentelo de nuevo..." << endl;
+        }
+
+    } while (username != "PM-202000558" || password != "pmPassword123");
+
+    return false;
+}
+
+
+void mostrarMenu()
+{
+    int opcion;
+
+    do {
+        cout << "\n\t=============      EDD PROJECT-UP     =============" << endl;
+        cout << "\t============= Bienvenido PM-202000558 =============\n" << endl;
+        cout << "\t1. Cargar Empleados" << endl;
+        cout << "\t2. Crear Proyecto" << endl;
+        cout << "\t3. Crear Tareas" << endl;
+        cout << "\t4. Asignar Tareas" << endl;
+        cout << "\t5. Salir" << endl;
+        cout << "\n\tElija una opcion: ";
+        cout << "";
+        cin >> opcion;
+
+        switch (opcion) {
+            case 1:
+                cout << "\tRealizar opcion 1" << endl;
+                break;
+
+            case 2:
+                cout << "\tRealizar opcion 2" << endl;
+                break;
+
+            case 3:
+                cout << "\tRealizar opcion 3" << endl;
+                break;
+
+            case 4:
+                cout << "\tRealizar opcion 4" << endl;
+                break;
+
+            default:
+                cout << "\n\topcion invalida. Intente de nuevo." << endl;
+                break;
+        }
+
+    } while (opcion != 5);
+}
+
+
+
